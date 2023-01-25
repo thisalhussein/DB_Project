8a48564bf3c8110b8c894ef592d686418c5addc3
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-25 14:53:41 +0200

    change pic position

--- DB_Project/Employee.Designer.cs
+++ DB_Project/Employee.Designer.cs
@@ -188,7 +188,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(338, 131);
+            this.pictureBox1.Location = new System.Drawing.Point(190, 110);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(72, 68);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
