95b25c730263e53ffd404f9c4e88bdae321d7c3b
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-25 15:39:30 +0200

    change form border style

--- DB_Project/salary.Designer.cs
+++ DB_Project/salary.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // salary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "salary";
             this.Text = "salary";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
