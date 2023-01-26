39f4e2f0218567117184084da097676c90fba5a3
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-26 10:12:56 +0200

    Change table name and PRIMARY IDENTITY

--- dbo.Table.sql
+++ dbo.Table.sql
@@ -1,5 +1,5 @@
-﻿CREATE TABLE [dbo].[Table]
+﻿CREATE TABLE [dbo].[DepartmentTbl]
 (
-	[DepId] INT NOT NULL PRIMARY KEY, 
+	[DepId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [DepName] NCHAR(100) NULL
 )
