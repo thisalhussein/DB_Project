f7af3f544de7099c3fc19abcedd1a2884e240ac4
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-26 10:30:32 +0200

    Add Another columns and make foreign keys

--- dbo.Table1.sql
+++ dbo.Table1.sql
@@ -1,7 +1,11 @@
-﻿CREATE TABLE [dbo].[Table]
+﻿CREATE TABLE [dbo].[EmloyeeTbl]
 (
-	[EmpId] INT NOT NULL PRIMARY KEY, 
-    [EmpName] NCHAR(50) NULL, 
-    [EmpGen] NCHAR(10) NULL, 
-    [EmpDep] INT NULL
+	[EmpId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [EmpName] NCHAR(50) NOT NULL, 
+    [EmpGen] NCHAR(10) NOT NULL, 
+    [EmpDep] INT NOT NULL, 
+    [EmpDDB] DATE NOT NULL, 
+    [EmpDate] DATE NOT NULL, 
+    [EmpSal] INT NOT NULL, 
+    CONSTRAINT [FK1] FOREIGN KEY ([EmpDep]) REFERENCES [DepartmentTbl]([DepId])
 )
