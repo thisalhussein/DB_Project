2ce7593122afe7809d9ac9addbca2050d9e210da
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-01-26 10:46:13 +0200

    add sql connection in C#

--- DB_Project/Function.cs
+++ DB_Project/Function.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace DB_Project
 {
     internal class Function
     {
+        private SqlConnection
         public Function()
         {
 
